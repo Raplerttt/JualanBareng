fee2d2fb55cde02604af1e876ddbe7b6d83d5606
mac <mac@macs-MacBook-Pro.local>
2025-08-05 18:56:17 +0700

    test

--- utils/axios.js
+++ utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://jualanbareng-api-e249c10b3bbe.herokuapp.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
@@ -12,7 +12,7 @@ const publicEndpoints = [
   '/product',
   '/categories',
   '/auth/login',
-  '/seller',
+  '/auth/register',
 ];
 
 // Simpan fungsi refreshAccessToken dari AuthContext
